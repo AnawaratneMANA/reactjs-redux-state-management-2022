1e6012da78ccfd03622231d0913d9c0ebc08ce62
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-21 16:21:00 +0530

    COMMIT: Redux operation added.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React from 'react';
-import {useSelector} from 'react-redux'
+import {useSelector, useDispatch} from 'react-redux'
 import './App.css';
 
 function App() {
   const counter = useSelector((state) => state.counter);
-
+  const dispatch = useDispatch();
   // Increment function - Increase Redux values.
   const increment = () => {
-      console.log("Increment")
+      dispatch({type: "INCREMENT"})
   };
   // Decrement function - Decrease Redux values.
   const decrement = () => {
-
+      dispatch({type: "DECREMENT" })
   };
 
+  const addby = () => {
+    dispatch({type: "ADD", payload: 10})
+  }
+
   return (
-    <div>
+    <div className='container'>
       <h1>Counter App Demo</h1>
       <h2>{counter}</h2>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      Increment the value in the redux state.
+      <button className='btn-success' onClick={increment}>Increment</button><br></br>
+      Decrement the value in the redux state.
+      <button className='btn-success' onClick={decrement}>Decrement</button><br></br>
+      Passing Payload to the redux state.
+      <button className='btn-success' onClick={addby}>Add By 10</button><br></br>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,19 @@
 import {createStore} from 'redux'
 
 const reducerFn = (state={counter:10}, action) => {
+    /*
+    Always Syncronous
+    Should not mutate the original state.
+    */
+   if (action.type === "INCREMENT"){
+       return {counter:state.counter + 1};
+   }
+   if(action.type === "DECREMENT"){
+       return {counter:state.counter - 1};
+   }
+   if(action.type === "ADD"){
+       return {counter: state.counter + action.payload};
+   }
     return state;
 }
 // This is not the reccomended approach for creating store.
