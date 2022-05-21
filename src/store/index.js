07179262861151ec6a36fc8c3968d1ff267cbd70
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-21 14:42:31 +0530

    COMMIT: Store added to the main app.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,11 @@
+// Method is not deprectated.
 import {createStore} from 'redux'
 
 const reducerFn = (state={counter:0}, action) => {
-    
+
 }
+// This is not the reccomended approach for creating store.
 const store = createStore(reducerFn);
+export default store;
+
+// https://react-redux.js.org/tutorials/quick-start
