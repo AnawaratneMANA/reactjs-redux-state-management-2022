f6f7cc1bff217e4fdc2260b787cb7d395e23db15
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-21 16:46:05 +0530

    COMMIT: Store method recreation with took-kit.

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,24 @@
-// Method is not deprectated.
-import {createStore} from 'redux'
+// Redux tool-kit method.
+import {configureStore, createSlice} from '@reduxjs/toolkit'
+const counterSlice = createSlice({
+    name: "counter",
+    initialState: {counter: 0},
+    reducers: {
+        increment(state, action){
+            state.counter + 1;
+        },
+        decrement(state, action){
+            state.counter - 1;
+        },
+        addby(state, action){
+            state.counter + 10;
+        }
+    }
+})
 
-const reducerFn = (state={counter:10}, action) => {
-    /*
-    Always Syncronous
-    Should not mutate the original state.
-    */
-   if (action.type === "INCREMENT"){
-       return {counter:state.counter + 1};
-   }
-   if(action.type === "DECREMENT"){
-       return {counter:state.counter - 1};
-   }
-   if(action.type === "ADD"){
-       return {counter: state.counter + action.payload};
-   }
-    return state;
-}
-// This is not the reccomended approach for creating store.
-const store = createStore(reducerFn);
-export default store;
+export const actions = counterSlice.actions;
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
 
 // https://react-redux.js.org/tutorials/quick-start
