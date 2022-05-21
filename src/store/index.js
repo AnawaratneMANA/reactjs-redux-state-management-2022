dbdb65046e433bf41c5816351e56482b794dc3b8
AnawaratneMANA <it19086408@my.sliit.lk>
2022-05-21 16:04:31 +0530

    COMMIT: Add store updates.

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@
 import {createStore} from 'redux'
 
 const reducerFn = (state={counter:0}, action) => {
-
+    return state;
 }
 // This is not the reccomended approach for creating store.
 const store = createStore(reducerFn);
